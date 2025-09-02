b196fdbb74dac5888892e1454f32b90c0233fd50
Avery Kuenzi <averyk348@gmail.com>
2025-09-02 09:01:58 -0400

    Three Projects in Java
    The three projects added showcase key points within Java programming, Showcasing Looping, Pattern Creation, and simple arithmetic.

---
+++ Rectangle.java
@@ -0,0 +1,17 @@
+public class Rectangle {
+    public static void main(String[] args) {
+        //Dimensions
+        double width = 5.5;
+        double height = 8.5;
+
+        //Area and Perimeter
+        double area = width * height;
+        double perimeter = 2 * (width + height);
+
+        //Results
+        System.out.println("Width: " + width);
+        System.out.println("Height: " + height);
+        System.out.println("Area: " + area);
+        System.out.println("Perimeter: " + perimeter);
+    }
+}
